56fda17658c2bae35deb7744ac6aa6779ad492d0
Mohamed Nasser <nasseratic@gmail.com>
2021-05-11 18:12:16 +0200

    feat: fix binding, date pick start/end of day

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,36 @@
 <script>
   import DatePicker from "./DatePicker.svelte";
 
+  const baseUrl = "https://intouch-analytics.azurewebsites.net";
+
+  const startDate = new Date();
+  startDate.setHours(0, 0, 0, 0);
+
+  const endDate = new Date();
+  endDate.setHours(23, 59, 59, 999);
+
   let dateRange = {
-    startDate: new Date(),
-    endDate: new Date(),
+    startDate,
+    endDate,
   };
 
   let placeId;
   let assetId;
 
-  const onDateChange = (key) => (d) => {
-    dateRange = { ...dateRange, [key]: d.detail };
+  $: url = `${baseUrl}/impressions/impressionContext?startDate=${dateRange.startDate.toISOString()}&endDate=${dateRange.endDate.toISOString()}${
+    placeId ? `&placeId=${placeId}` : ""
+  }${assetId ? `&assetId=${assetId}` : ""}`;
+
+  $: console.log(`the count is ${url}`);
+
+  const onDateChangeStartDate = (d) => {
+    d.detail.setHours(0, 0, 0, 0);
+    dateRange = { ...dateRange, startDate: d.detail };
+  };
+
+  const onDateChangeEndDate = (d) => {
+    d.detail.setHours(23, 59, 59, 999);
+    dateRange = { ...dateRange, endDate: d.detail };
   };
 </script>
 
@@ -18,28 +38,24 @@
 
 <div class="picker-container">
   <DatePicker
-    on:datechange={onDateChange("startDate")}
+    on:datechange={onDateChangeStartDate}
     selected={dateRange.startDate}
   />
   <DatePicker
-    on:datechange={onDateChange("endDate")}
+    on:datechange={onDateChangeEndDate}
     selected={dateRange.endDate}
   />
 </div>
 
 <div>
-  <input value={placeId} type="number" placeholder="Place ID" />
+  <input bind:value={placeId} type="number" placeholder="Place ID" />
 </div>
 <div>
-  <input value={assetId} type="number" placeholder="Asset ID" />
+  <input bind:value={assetId} type="number" placeholder="Asset ID" />
 </div>
 
 <h3>
-  <a
-    href={`http://intouch-dev-druid.northeurope.cloudapp.azure.com/impressions/impressionContext?startDate=${dateRange.startDate.toISOString()}&endDate=${dateRange.endDate.toISOString()}${
-      placeId ? `&placeId=${placeId}` : ""
-    }${assetId ? `&assetId=${assetId}` : ""}`}>Download</a
-  >
+  <a href={url}>Download</a>
 </h3>
 
 <style>
