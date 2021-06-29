d2ce9e54b743cc4914e3029c3ca6aa3ef39cb653
Mohamed Nasser <nasseratic@gmail.com>
2021-06-29 14:54:14 +0200

    fix: change url env for places assets

--- src/placesAssetsApi.js
+++ src/placesAssetsApi.js
@@ -1,6 +1,11 @@
-export const getPlacesAssets = async (key) => {
+const url = {
+  prod: "https://intouchapis.azure-api.net/api/4n4lytics/place-assets",
+  stg: "https://stg-intouchapis.azure-api.net/api/4n4lytics/place-assets",
+};
+
+export const getPlacesAssets = async (env,key) => {
   const Authorization =  `Basic ${btoa(key)}`;
-  return await (await fetch("https://dev-intouchapis.azure-api.net/api/4n4lytics/place-assets",{headers:{
+  return await (await fetch(url[env],{headers:{
     "Ocp-Apim-Subscription-Key":"b21661c1e9f14136aff76eb2fec8580e",
     Authorization
   }})).json()
