90fe38b6bd207b22bdf970dd0940f16bae06f1f5
Mohamed Nasser <nasseratic@gmail.com>
2021-07-28 15:06:51 +0200

    feat: refactor, add withContext option in context report

---
+++ src/components/Label.svelte
@@ -0,0 +1,11 @@
+<script>
+  export let text;
+  export let forInput;
+</script>
+
+<label
+  class="block text-gray-500 text-sm font-lite mb-2 uppercase"
+  for={forInput}
+>
+  {text}
+</label>
--- src/pages/ImpressionsReport.svelte
+++ src/pages/ImpressionsReport.svelte
@@ -3,53 +3,20 @@
   import Spinner from "../components/Spinner.svelte";
   import { getPlacesAssets } from "../utils/placesAssetsApi";
   import { user } from "../store";
-
-  let env = "prod";
-
-  const baseUrl = {
-    prod: process.env.IMPRESSIONS_REPORT_URL,
-    stg: process.env.IMPRESSIONS_REPORT_URL_STG,
-  };
+  import moment from "moment";
+  import Label from "../components/Label.svelte";
+  import { text } from "svelte/internal";
 
   const startDate = new Date();
 
   const endDate = new Date();
 
+  let env = "prod";
+
   let dateRange = {
     startDate,
     endDate,
   };
-
-  let placeId;
-  let assetId;
-  let startDateStr = "";
-  let endDateStr = "";
-  let url = "";
-
-  $: {
-    let month = "" + (dateRange.startDate.getMonth() + 1),
-      day = "" + dateRange.startDate.getDate(),
-      year = dateRange.startDate.getFullYear();
-
-    if (month.length < 2) month = "0" + month;
-    if (day.length < 2) day = "0" + day;
-
-    startDateStr = [year, month, day].join("-") + "T00:00:00.000Z";
-
-    month = "" + (dateRange.endDate.getMonth() + 1);
-    day = "" + dateRange.endDate.getDate();
-    year = dateRange.endDate.getFullYear();
-
-    if (month.length < 2) month = "0" + month;
-    if (day.length < 2) day = "0" + day;
-
-    endDateStr = [year, month, day].join("-") + "T23:59:59.999Z";
-
-    url = `${baseUrl[env]}?startDate=${startDateStr}&endDate=${endDateStr}${
-      placeId ? `&placeId=${placeId}` : ""
-    }${assetId ? `&assetId=${assetId}` : ""}`;
-    console.log(url);
-  }
 
   const onDateChangeStartDate = (d) => {
     dateRange = { ...dateRange, startDate: d.detail };
@@ -58,6 +25,25 @@
   const onDateChangeEndDate = (d) => {
     dateRange = { ...dateRange, endDate: d.detail };
   };
+
+  let placeId;
+  let withContext = false;
+  let assetId;
+  let url = "";
+
+  const baseUrl = {
+    prod: process.env.IMPRESSIONS_REPORT_URL,
+    stg: process.env.IMPRESSIONS_REPORT_URL_STG,
+  };
+
+  $: {
+    const start = moment(startDate).format("YYYY-MM-DD") + "T00:00:00.000Z";
+    const end = moment(endDate).format("YYYY-MM-DD") + "T23:59:59.999Z";
+
+    url = `${baseUrl[env]}?startDate=${start}&endDate=${end}&withContext=${withContext}`;
+    if (placeId) url = `${url}&placeId=${placeId}`;
+    if (assetId) url = `${url}&assetId=${assetId}`;
+  }
 
   $: placeAssetsData = getPlacesAssets(env, $user);
 </script>
@@ -70,50 +56,46 @@
       Impressions Report
     </h1>
     <div>
-      <label
-        class="block text-gray-500 text-sm font-lite mb-2 uppercase"
-        for="assetId"
-        >Env
-      </label>
+      <Label text="Enviroment" forInput="env" />
       <select
         class="shadow appearance-none bg-white w-80 border rounded py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
         bind:value={env}
+        id="env"
       >
         <option value="prod">Production</option>
         <option value="stg">Staging</option>
       </select>
     </div>
     <div>
-      <label
-        class="block text-gray-500 text-sm font-lite mb-2 uppercase"
-        for="start"
-        >From
-      </label>
+      <Label text="From Date" forInput="start" />
       <DatePicker
         on:datechange={onDateChangeStartDate}
         selected={dateRange.startDate}
       />
-      <label
-        class="block text-gray-500 text-sm font-lite mb-2 uppercase"
-        for="end"
-        >To
-      </label>
+
+      <Label text="To Date" forInput="end" />
       <DatePicker
         on:datechange={onDateChangeEndDate}
         selected={dateRange.endDate}
       />
     </div>
-
+    <div>
+      <Label text="With Context" forInput="withContext" />
+      <select
+        class="shadow appearance-none bg-white w-80 border rounded py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+        bind:value={withContext}
+        placeholder="With Context"
+        id="withContext"
+      >
+        <option value={false}>False</option>
+        <option value={true}>True</option>
+      </select>
+    </div>
     {#await placeAssetsData}
       <Spinner />
     {:then data}
       <div>
-        <label
-          class="block text-gray-500 text-sm font-lite mb-2 uppercase"
-          for="placeId"
-        >
-          Place ID
-        </label>
+        <Label text="Place Id" forInput="placeId" />
         <select
           class="shadow appearance-none bg-white w-80 border rounded py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
           bind:value={placeId}
@@ -127,11 +109,8 @@
         </select>
       </div>
       <div>
-        <label
-          class="block text-gray-500 text-sm font-lite mb-2 uppercase"
-          for="assetId"
-          >Asset ID
-        </label>
+        <Label text="Asset ID" forInput="assetId" />
+
         <select
           class="shadow appearance-none bg-white w-80 border rounded py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
           bind:value={assetId}
